4e4a659b92052c24f282144015fbdd8dc492dde3
TwDDarkKill™ <Niltondark53@hotmail.com>
2023-06-07 16:59:32 -0300

    07/06

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ ScrollReveal().reveal('.container',{duration: 2000});
 
 ScrollReveal().reveal('.creditos',{duration: 2750});
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
   
   
   
